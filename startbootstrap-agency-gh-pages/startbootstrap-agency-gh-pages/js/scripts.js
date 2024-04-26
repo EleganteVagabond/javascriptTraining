744e77c9a842b46e3707fea17e6b7f528b62f981
Drew Elegante <hpogt1@gmail.com>
2024-04-26 16:17:29 -0600

    Initial add to Repo

--- startbootstrap-agency-gh-pages/startbootstrap-agency-gh-pages/js/scripts.js
+++ startbootstrap-agency-gh-pages/startbootstrap-agency-gh-pages/js/scripts.js
@@ -7,7 +7,7 @@
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    $('a.js-scroll-trigger').trigger(function () {
         if (
             location.pathname.replace(/^\//, "") ==
                 this.pathname.replace(/^\//, "") &&
@@ -49,8 +49,8 @@
             $("#mainNav").removeClass("navbar-shrink");
         }
     };
-    // Collapse now if page is not at top
-    navbarCollapse();
+    // Collapse IFF document is ready
+    $(document).ready(navbarCollapse());
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
